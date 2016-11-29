9b534eac0b31077393f90a59934d42b377bb475e
高扬 <gaoyang@kous-MacBook.local>
2016-11-29 12:21:01 +0200

    Initial commit

---
+++ todo-list/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+<h1>
+  Hello {{opmName}} {{title}}
+</h1>
+`,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'OPM works!';
+  opmName = 'OPM rocks';
+}
